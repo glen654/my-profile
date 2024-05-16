2071f849e0422784b66ddc431e671ec4b25baba9
Glen Alloy <glenalloy8@gmail.com>
2024-05-16 21:19:18 +0530

    hamburger menu styles done

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-menu').addEventListener('click', function () {
+    var hamburger = document.getElementById('hamburger');
+    hamburger.style.display = (hamburger.style.display === 'none') ? 'block' : 'none';
+});
