0ce5af884eb4bf92881262dcc80445a7716cf38d
Glen Alloy <glenalloy8@gmail.com>
2024-05-19 10:39:18 +0530

    few changes in js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 document.getElementById('hamburger-menu').addEventListener('click', function () {
     var hamburger = document.getElementById('hamburger');
+    var icon = document.getElementById('hamburger-icon');
+    var close = document.getElementById('x-icon');
+
     hamburger.style.display = (hamburger.style.display === 'none') ? 'block' : 'none';
+    icon.style.display = (icon.style.display === 'block') ? 'none' : 'block';
+    close.style.display = (close.style.display === 'none') ? 'block' : 'none';
 });
