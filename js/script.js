6a4c7d97909a354eda2c73f3dd13799139a42714
glen654 <glenalloy@gmail.com>
2025-01-18 21:29:11 +0530

    modifying the hero section

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-document.getElementById('hamburger-menu').addEventListener('click', function () {
-    var hamburger = document.getElementById('hamburger');
-    var icon = document.getElementById('hamburger-icon');
-    var close = document.getElementById('x-icon');
+// document.getElementById('hamburger-menu').addEventListener('click', function () {
+//     var hamburger = document.getElementById('hamburger');
+//     var icon = document.getElementById('hamburger-icon');
+//     var close = document.getElementById('x-icon');
 
-    hamburger.style.display = (hamburger.style.display === 'none') ? 'block' : 'none';
-    icon.style.display = (icon.style.display === 'block') ? 'none' : 'block';
-    close.style.display = (close.style.display === 'none') ? 'block' : 'none';
-});
+//     hamburger.style.display = (hamburger.style.display === 'none') ? 'block' : 'none';
+//     icon.style.display = (icon.style.display === 'block') ? 'none' : 'block';
+//     close.style.display = (close.style.display === 'none') ? 'block' : 'none';
+// });
 
-document.addEventListener("DOMContentLoaded", function() {
-    window.onload = function() {
-        setTimeout(function() {
-            document.getElementById('preloader').style.display = 'none';
-            document.getElementById('home').style.display = 'block';
-        }, 1800);
-    };
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     window.onload = function() {
+//         setTimeout(function() {
+//             document.getElementById('preloader').style.display = 'none';
+//             document.getElementById('home').style.display = 'block';
+//         }, 1800);
+//     };
+// });
 
